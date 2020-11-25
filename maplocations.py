e7becc2199de2293f120a1fb694f9425ed29ef0f
are <areifeoluwa10@gmail.com>
2020-11-25 09:10:11 +0100

    added abuja map centres

--- maplocations.py
+++ maplocations.py
@@ -72,5 +72,104 @@ LOCATIONS = {
                 "name":"Muyiwa Ogunmade"
             }
         ]
+    },
+    "abuja":{
+        "amac":[
+            {
+                "location":"lugbe",
+                "contact":"08143140914",
+                "name":"Ajifa Ali"
+            },
+            {
+                "location":"lugbe",
+                "contact":"08164660697",
+                "name":"Philip Somiari"
+            },
+            {
+                "location":"lokogoma",
+                "contact":"08081822074",
+                "name":"Goodness"
+            },
+            {
+                "location":"lokogoma",
+                "contact":"07030936430",
+                "name":"Anselm"
+            },
+            {
+                "location":"Gudu/Apo Legislative Quaters",
+                "contact":"07035294089",
+                "name":"Damilola"
+            },
+            {
+                "location":"maitama/gishiri",
+                "contact":"07062651122",
+                "name":"Emaediong"
+            },
+            {
+                "location":"maitama/gishiri",
+                "contact":"08130475372",
+                "name":"Imaobong"
+            },
+            {
+                "location":"garki",
+                "contact":"08179335521",
+                "name":"Chinedu"
+            },
+            {
+                "location":"Wuse",
+                "contact":"08024266962",
+                "name":"Ukeme"
+            },
+            {
+                "location":"Lifecamp/Kado/Jabi",
+                "contact":"08077195219",
+                "name":"Chito Nwigene"
+            },
+            {
+                "location":"wuye/utako",
+                "contact":"08020579122",
+                "name":"Paul Usikpo"
+            },
+            {
+                "location":"wuye/utako",
+                "contact":"08069839038",
+                "name":"Edozie"
+            },
+            {
+                "location":"gwarinpa/dawaki",
+                "contact":"08052464065",
+                "name":"Joan Onotu"
+            },
+            {
+                "location":"gwarinpa/dawaki",
+                "contact":"08138533068",
+                "name":"Kemi"
+            },
+            {
+                "location":"nyanya/Maraba",
+                "contact":"07066700121",
+                "name":"Lydia Uwota"
+            },
+            {
+                "location":"nyanya/Maraba",
+                "contact":"08133464118",
+                "name":"Chioba"
+            },
+            {
+                "location":"kubwa/Dei-dei",
+                "contact":"07066813520",
+                "name":"Opeyemi"
+            },
+            {
+                "location":"kubwa/Dei-dei",
+                "contact":"08166751719",
+                "name":"Kelvin"
+            },
+            {
+                "location":"Suleja/Mandalla",
+                "contact":"08094165089",
+                "name":"Kingsley"
+            }
+        ]
     }
 }
